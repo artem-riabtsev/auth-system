09ae14952dbdbecb3e254730c241592059b831c7
Артем Рябцев <artem@riabtsev.ru>
2025-12-02 23:53:39 +0300

    feat: Permission checking

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'users.middleware.JWTAuthenticationMiddleware',
+    'permissions.middleware.PermissionMiddleware',
 ]
 
 ROOT_URLCONF = 'auth_system.urls'
--- permissions/views.py
+++ permissions/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .utils import PermissionChecker
 
-# Create your views here.
+
+class CheckPermissionView(APIView):
+    """API для проверки прав пользователя"""
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        permissions = PermissionChecker.get_user_permissions(request.user)
+        user_roles = list(request.user.roles.values_list('name', flat=True))
+        
+        return Response({
+            'user_id': request.user.id,
+            'email': request.user.email,
+            'roles': user_roles,
+            'permissions': permissions
+        })
+
+
+class TestPermissionView(APIView):
+    """Тестовый view для проверки конкретного права"""
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        # Проверяем право вручную
+        has_perm = PermissionChecker.has_permission(
+            user=request.user,
+            element_code='users',
+            permission_code='read'
+        )
+        
+        if not has_perm:
+            return Response(
+                {'detail': 'У вас нет прав для выполнения этого действия'},
+                status=403
+            )
+        
+        return Response({
+            'message': 'Доступ разрешен! У вас есть право read на users',
+            'user': request.user.email
+        })
